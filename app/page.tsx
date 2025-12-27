c587bc8e48775125911f4a2f197bd5ae49c76364
uchest <uchenweke2000@yahoo.com>
2025-12-27 04:03:30 +0100

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import EventCard from '@/components/EventCard';
 import ExploreBtn from '@/components/ExploreBtn';
 import { cacheLife } from 'next/cache';
-import { events } from '@/lib/constants';
+
+import { IEvent } from '@/database';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async () => {
   'use cache';
   cacheLife('hours');
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
   return (
     <section>
